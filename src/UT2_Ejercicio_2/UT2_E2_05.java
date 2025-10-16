42d3283bce02c632cabf6bc7a1e52722af2fd4bc
Natalia <natalia.marcos@educa.jcyl.es>
2025-10-16 14:35:00 +0200

    Finalizado: UT2_E2_05, ATENCION A LOS PRINT/PRINTNL

--- src/UT2_Ejercicio_2/UT2_E2_05.java
+++ src/UT2_Ejercicio_2/UT2_E2_05.java
@@ -1,7 +1,8 @@
 package UT2_Ejercicio_2;
+import java.util.Scanner;
 
 public class UT2_E2_05 {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         // Desarrolla un programa que solicite un número n y luego muestre por pantalla la siguiente figura:
         //1
         //1 2
@@ -11,6 +12,17 @@ public class UT2_E2_05 {
         //………….
         //1 2 3 4 5 …..n
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un número");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
 
     }
 }
