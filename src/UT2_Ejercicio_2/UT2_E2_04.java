797fbc18df733f6ce4766b3736e046e9a4213506
Natalia <natalia.marcos@educa.jcyl.es>
2025-10-16 14:25:48 +0200

    Finalizado: UT2_E2_04

--- src/UT2_Ejercicio_2/UT2_E2_04.java
+++ src/UT2_Ejercicio_2/UT2_E2_04.java
@@ -1,7 +1,28 @@
 package UT2_Ejercicio_2;
 
+import java.util.Scanner;
+
 public class UT2_E2_04 {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         //Desarrolla un programa que pida un número por teclado y te muestre el número de cifras que tiene.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un numero entero para saber cuantas cifras tiene");
+        int n = sc.nextInt();
+        int contador = 0;
+
+        if (n < 0) {
+            n = n * -1;
+        }
+        if (n == 0){
+            System.out.println("El numero elegido tiene una cifra");
+        } else {
+            while (n > 0) {
+                n = n / 10;
+                contador++;
+            }
+            System.out.println("El numero elegido tiene " + contador + " cifras");
+        }
+
+        sc.close();
     }
 }
