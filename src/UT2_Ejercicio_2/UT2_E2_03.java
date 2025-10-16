2382d2e4775a84191af03e6552df8e72cd4ec006
Natalia <natalia.marcos@educa.jcyl.es>
2025-10-16 14:11:40 +0200

    Finalizado: UT2_E2_03

--- src/UT2_Ejercicio_2/UT2_E2_03.java
+++ src/UT2_Ejercicio_2/UT2_E2_03.java
@@ -1,28 +1,32 @@
 package UT2_Ejercicio_2;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class UT2_E2_03 {
-    static void main(String[] args) {
-        // Crea un programa en el que introduzcas una nota del 1 al 10 y te indique su nota en formato escrito (insuficiente, suficiente, etc.).
+   public static void main(String[] args) {
+       // Crea un programa en el que introduzcas una nota del 1 al 10 y te indique su nota en formato escrito (insuficiente, suficiente, etc.).
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca un número entre 1 y 10, para saber si su nota es suficiente, insuficiente...");
-        int n = sc.nextInt();
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Introduzca un número entre 1 y 10, para saber si su nota es suficiente, insuficiente...");
+       int n = sc.nextInt();
 
-        if ( n < 1 && n > 10);{
-        System.out.println(" El número elegido no es valido");
-        n = sc.nextInt();
-        } else if (n > 1 && n < 5 ){
-            System.out.println(" Su nota es insuficiente ");
-        } else if (n > 5 && n < 7 ) {
-            System.out.println(" Su nota es suficiente  ");
-        } else if (n > 7 && n < 9 ){
-            System.out.println(" Su nota es notable ");
-        } else {
-            System.out.println( "Su nota es sobresaliente");
-        }
+       if (n < 1 || n > 10) {
+           System.out.println(" El número elegido no es valido");
+       } else {
+           if (n >= 1 && n < 5) {
+               System.out.println("Su nota es insuficiente.");
+           } else if (n == 5) {
+               System.out.println("Su nota es suficiente.");
+           } else if (n == 6) {
+               System.out.println("Su nota es bien.");
+           } else if (n == 7 || n == 8) {
+               System.out.println("Su nota es notable.");
+           } else if (n == 9 || n == 10) {
+               System.out.println("Su nota es sobresaliente.");
+           }
+       }
 
-        sc.close();
-    }
-}
+           sc.close();
+       }
+   }
