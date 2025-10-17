f2043eff7b8dc11f6e45288657bf5253a93ff93c
Natalia <natalia.marcos@educa.jcyl.es>
2025-10-17 23:39:31 +0200

    Finalizado: UT2_E2_08

--- src/UT2_Ejercicio_2/UT2_E2_08.java
+++ src/UT2_Ejercicio_2/UT2_E2_08.java
@@ -1,9 +1,25 @@
 package UT2_Ejercicio_2;
 
 public class UT2_E2_08 {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         // Muestra los números primos entre 1 y 100.
 
+        System.out.println(" Los números primos  entre 1 y 100 son: ");
+
+        for(int i = 2; i <=100; i++){
+            boolean primo = true;
+
+            for (int j = 2; j< i; j++) {
+                if (i % j == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+        if (primo) {
+                    System.out.println(i);
+                }
+            }
+        }
 
     }
-}
+
