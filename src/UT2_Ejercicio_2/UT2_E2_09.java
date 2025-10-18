6c983902bfabf94aa0db53d67db5076985c28f91
Natalia <natalia.marcos@educa.jcyl.es>
2025-10-18 09:54:32 +0200

    Finalizado: UT2_E2_09

--- src/UT2_Ejercicio_2/UT2_E2_09.java
+++ src/UT2_Ejercicio_2/UT2_E2_09.java
@@ -1,7 +1,26 @@
 package UT2_Ejercicio_2;
 
+import java.util.Scanner;
+
 public class UT2_E2_09 {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         // Mostrar los N primeros términos de la serie de Fibonacci. N se definirá como una variable.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cuántos números de la serie de Fibonacci quieres ver?");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(a + " ");
+
+            int siguiente = a + b;
+            a = b;
+            b = siguiente;
+        }
+
+        sc.close();
+
     }
 }
