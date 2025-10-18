da89cb96d57a643501d190ac765535b4f32d922e
Natalia <natalia.marcos@educa.jcyl.es>
2025-10-18 10:01:21 +0200

    Finalizado: UT2_E2_10

--- src/UT2_Ejercicio_2/UT2_E2_10.java
+++ src/UT2_Ejercicio_2/UT2_E2_10.java
@@ -1,9 +1,24 @@
 package UT2_Ejercicio_2;
 
+import java.util.Scanner;
+
 public class UT2_E2_10 {
     static void main(String[] args) {
         // Igual que el anterior, pero el valor de N se leerá por teclado.
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cuántos números de la serie de Fibonacci quieres ver?");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
 
+        for (int i = 1; i <= n; i++){
+            System.out.println( a + " ");
+            int siguiente = a+b;
+            a = b;
+            b = siguiente;
+        }
+
+        sc.close();
     }
 }
